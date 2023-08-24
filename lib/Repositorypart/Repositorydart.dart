749c904f971b37bae02fcc737fb221ebb6c5dac5
Dr. Raed <drraed.jb@gmail.com>
2023-08-24 12:26:55 +0200

    RepositoryDart

---
+++ lib/Repositorypart/Repositorydart.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:tutorialflutterbloc/modelpart/photodart.dart';
+import 'package:http/http.dart' as http;
+class RepositoryDart{
+  String urlforphotos="https://jsonplaceholder.typicode.com/photos";
+  Future<List<PhotoDart>> gettingAllPhotos() async{
+     http.Response responsing=await http.get(Uri.parse(urlforphotos));
+    if(responsing.statusCode==200)
+    {
+      final List result=jsonDecode(responsing.body);
+      return result.map((e) => PhotoDart.fromJsonData(e)).toList();
+    }
+    else
+    {
+      throw Exception(responsing.statusCode.toString());
+    }
+  }
+}
